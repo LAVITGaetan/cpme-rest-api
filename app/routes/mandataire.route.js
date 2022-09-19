2df35e029127f6d27a78fcc00a5994de43be3bf0
Gaetan LAVIT <g.lavit@outlook.fr>
2022-09-19 21:50:08 +0400

    Possibilité d' ajouter une image pour un mandataire (API ET CLIENT)

--- app/routes/mandataire.route.js
+++ app/routes/mandataire.route.js
@@ -2,6 +2,32 @@ const express = require('express')
 const router = express.Router();
 const Mandataire = require('../models/mandataire.model');
 
+// MULTER Settings
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/uploads')
+    },
+    filename: function (req, file, cb) {
+        cb(null, `logo_${req.body.nom}_${req.body.prenom}_${file.originalname}`)
+    }
+})
+const fileFilter = function (req, file, cb) {
+
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+        cb(null, true)
+    }
+    else {
+        cb(null, false)
+    }
+}
+const upload = multer({
+    storage: storage, limits: {
+        fileSize: 1024 * 1024 * 5,
+    },
+    fileFilter: fileFilter
+});
+
 // Retrieve all mandataires
 router.get('/', async (req, res) => {
     try {
@@ -23,8 +49,19 @@ router.get('/:id', async (req, res) => {
 })
 
 // Add mandataire
-router.post('/', async (req, res) => {
-    const mandataire = new Mandataire(req.body);
+router.post('/', upload.single('mandataireLogo'), async (req, res) => {
+    if (req.file) {
+        path = req.file.path.substring(7)
+    }
+    else {
+        path = req.body.logo || 'none'
+    }
+    const mandataire = new Mandataire({
+        nom: req.body.nom,
+        prenom: req.body.prenom,
+        description: req.body.description,
+        logo: path
+    });
     try {
         await mandataire.save();
         res.send(mandataire)
